32c03e8f34e47b5114ba05375a8a49e3c687b8f1
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-10-22 23:22:30 +0600

    another commit

--- index.js
+++ index.js
@@ -16,33 +16,37 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-/* client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-}); */
-
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const products00Collection = client.db('laptop_store00_db').collection('products00');
 
         /////////////////
-        // add product api
-        app.get('/products', async(req, res) =>{
+        // get product api
+        app.get('/products', async (req, res) => {
             const query = {};
-        })
+            const cursor = products00Collection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        //post product api
+        app.post('/products', async (req, res) => {
+            const newProdcut = req.body;
+            const result = await productCollection.insertOne(newProdcut);
+            res.send(result);
+        });
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send(('hello from lapto_store00_server'))
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`laptop_store00_server app listening on port${port})`)
 })
